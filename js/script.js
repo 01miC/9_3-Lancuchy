f90cd5ebf4f464e50903d2df5b9204dc1326c158
Michal Dokupil <michal@dokupil.pl>
2017-12-28 22:43:16 +0100

    Add script.js with complete task 9.3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-var femaleNames = ['Asia', 'Kasia', 'Monika', 'Sylwia', 'Jadwiga', 'Ania'];
-var maleNames = [ 'Tomek', 'Wojtek', 'Jakub', 'Kacper', 'Marcin' ];
-var allNames = femaleNames.concat(maleNames);
-var newName = 'Marian';
+var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
+var dinosaur =  'triceratops';
+var dinosaurUpperCase = dinosaur.toUpperCase();
+var textCorrected = text.replace('Velociraptor', dinosaurUpperCase);
+    var half = textCorrected.length/2;
+    var officalText = textCorrected.slice(0, half);
 
-console.log(allNames);
-if (allNames.indexOf(newName) === -1 ) {
-    allNames.push(newName);
-    console.log(allNames);
-}else {
-    console.log('Zle dane');
-}
+console.log(text);
+console.log('Change the dinosaur with', dinosaur);
+console.log(textCorrected);
+console.log('');
+console.log(officalText);
